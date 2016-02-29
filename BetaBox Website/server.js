b1f493cef9ddbcc4250b024b85488a0a421ac72e
Justin Reigel <justinreigel89@gmail.com>
2016-02-29 00:52:15 -0700

    Moved server and package outside, added Readme
    Added readme for node server instructions (updated footer will not run
    without a server.)

--- BetaBox Website/server.js
+++ BetaBox Website/server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 var http = require('http');
 var httpServer = http.Server(app);
-app.use(express.static(__dirname));
+app.use(express.static(__dirname+'/Pages'));
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/home.html');
+  res.sendFile(__dirname + '/Pages/home.html');
 });
 app.listen(3000);
