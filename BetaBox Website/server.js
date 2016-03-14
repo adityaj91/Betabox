030f0562677fe396c51de7c69ecfd38920663483
Justin Reigel <justinreigel89@gmail.com>
2016-03-13 23:18:42 -0700

    Updated Server
    Server can now be created with a specified port and displays appropriate
    success/error messages.

--- BetaBox Website/server.js
+++ BetaBox Website/server.js
@@ -6,4 +6,30 @@ app.use(express.static(__dirname+'/Pages'));
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/Pages/home.html');
 });
-app.listen(3000);
+try{
+	// if you want to enter a manual port.
+	var args = Number(process.argv.slice(2));
+	if(isNaN(args)){
+    	throw new Error("Not a number");
+  	}
+	server = app.listen(args, function() {
+    	console.log('Ready on port %d', server.address().port);
+    	console.log("Server listening on http://localhost:%s", args);
+	});
+  	server.once('error', function(err) {
+  	  console.log("Server in use");
+  	  	// This is a messy paste, I had issues getting this error to the outer catch block
+		var redirectPort = 3000;
+		app.listen(redirectPort);
+		console.log("Default arg " + args + " is invalid or in use. \nredirect\n"+
+			"Server listening on http://localhost:" + redirectPort);
+		});
+}
+catch(err){
+	console.log(err);
+	var redirectPort = 3000;
+	app.listen(redirectPort);
+	console.log("Default arg " + args + " is invalid or in use. \nredirect\n"+
+		"Server listening on http://localhost:" + redirectPort);
+
+}
