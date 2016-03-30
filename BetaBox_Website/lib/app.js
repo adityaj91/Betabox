c5e6f4295f7022fcabb6258e97e5b00f42d89c09
aamannon <aamannon@asu.edu>
2016-03-30 12:05:18 -0700

    playing with connecting to server

--- BetaBox_Website/lib/app.js
+++ BetaBox_Website/lib/app.js
@@ -1,12 +1,12 @@
 //not working right now
-//ip: 104.236.168.166
-//username: root
-//password: Betab0x
-//db to test for now is users
+// ip= 104.236.168.166
+// username= root
+// password= Betab0x
+// db to test for now is users
 
 var MongoClient = require('mongodb').MongoClient
     , format = require('util').format;
-MongoClient.connect('mongodb://username:password@104.236.168.166/users', function (err, db) {
+MongoClient.connect('mongodb://104.236.168.166:27017/users', function (err, db) {
     if (err) {
         throw err;
     } else {
@@ -14,3 +14,13 @@ MongoClient.connect('mongodb://username:password@104.236.168.166/users', functio
     }
     db.close();
 });
+// 'mongodb:root:Betab0x@104.236.168.166/users'
+// // Retrieve
+// var MongoClient = require('mongodb').MongoClient;
+
+// // Connect to the db
+// MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
+//   if(!err) {
+//     console.log("We are connected");
+//   }
+// });
