23a5ab28b0716a735d5519274a58c57e9429022a
aamannon <aamannon@asu.edu>
2016-03-30 15:24:44 -0700

    small updates
    changed px to % to make is accurate in all screens

--- BetaBox_Website/lib/app.js
+++ BetaBox_Website/lib/app.js
@@ -4,16 +4,41 @@
 // password= Betab0x
 // db to test for now is users
 
-var MongoClient = require('mongodb').MongoClient
-    , format = require('util').format;
-MongoClient.connect('mongodb://root:Betab0x@104.236.168.166:27017/users', function (err, db) {
-    if (err) {
-        throw err;
-    } else {
-        console.log("successfully connected to the database");
-    }
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+var url = 'mongodb://root:Betab0x@104.236.168.166:27017/mongo';
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    //HURRAY!! We are connected. :)
+    console.log('Connection established to', url);
+
+    // do some work here with the database.
+
+    //Close connection
     db.close();
+  }
 });
+
+
+// var MongoClient = require('mongodb').MongoClient
+//     , format = require('util').format;
+// MongoClient.connect('mongodb://root:Betab0x@104.236.168.166:27017', function (err, db) {
+//     if (err) {
+//         throw err;
+//     } else {
+//         console.log("successfully connected to the database");
+//     }
+//     db.close();
+// });
 // 'mongodb:root:Betab0x@104.236.168.166/users'
 // // Retrieve
 // var MongoClient = require('mongodb').MongoClient;
