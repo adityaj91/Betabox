aa0546ac5cadc19e75bf4ab96bfda0002dbc5a7a
jreigel <justinreigel@yahoo.com>
2016-04-24 17:58:32 -0700

    Added Node Modules and MySQL post in server.js

--- BetaBox Website/server.js
+++ BetaBox Website/server.js
@@ -1,11 +1,45 @@
 var express = require('express');
 var app = express();
 var http = require('http');
+var body = require('body-parser');
 var httpServer = http.Server(app);
+
+var mysql = require("mysql");
+
+// First you need to create a connection to the db
+var con = mysql.createConnection({
+  host: "104.131.132.179",
+  user: "root",
+  password: "Betab0x"
+});
+
+con.connect(function(err){
+  if(err){
+    console.log('Error connecting to Db' + err);
+    return;
+  }
+  console.log('Connection established');
+});
+
+con.end(function(err) {
+  // The connection is terminated gracefully
+  // Ensures all previously enqueued queries are still
+  // before sending a COM_QUIT packet to the MySQL server.
+});
 app.use(express.static(__dirname+'/Pages'));
+
+app.post('/home.html', function(req, res){
+	// post the code!
+	// var username = req.body.login;
+	// var pass = req.body.password;
+	// sql.getTuple("users", username, pass);
+  res.sendFile(__dirname + '/Pages/home.html');
+});
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/Pages/home.html');
 });
+
 try{
 	// if you want to enter a manual port.
 	var args = Number(process.argv.slice(2));
